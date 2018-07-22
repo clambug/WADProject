b2c9536eaf537d0502528798f57c813b337f0b68
gaughan.robert <gaughan.robert@gmail.com>
2018-07-22 12:17:53 -0400

    db connected

--- app.js
+++ app.js
@@ -3,12 +3,36 @@ var app = express();
 const path = require('path');
 const VIEWS = path.join(__dirname, 'views');
 
-app.set('view engine', 'jade');
+app.set("view engine", "jade");
+
+var mysql = require('mysql'); // access to sql
 
 app.use(express.static("scripts"));  // allow the app access the scripts folder contents
 app.use(express.static("images"));   // allow the app access the images folder contents
 
 // function to set up a simple hello response 
+
+const db = mysql.createConnection({
+  
+  host: 'den1.mysql4.gear.host',
+  user: 'peakdb1',
+  password: 'Ic18_AG0~g8H',
+  database: 'peakdb1'
+  
+});
+
+db.connect((err) => {
+  if(err){
+    console.log("Database Connection Error")
+   // throw(err)  // Un comment this to throw the error rather than just log "You broke it"
+  }
+  else{
+  
+  console.log("Database Connected!!")
+  }
+});
+
+
 
 app.get('/', function(req, res){
  // res.send("Hello cruel world!"); // This is commented out to allow the index view to be rendered
