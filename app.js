491f10021c896c42808762f2dc3c245a797ea47b
gaughan.robert <gaughan.robert@gmail.com>
2018-08-09 13:01:33 -0400

    reviews

--- app.js
+++ app.js
@@ -2,15 +2,23 @@ var express = require("express"); // call express to be used by the application.
 var app = express();
 const path = require('path');
 const VIEWS = path.join(__dirname, 'views');
-
+var fs = require('fs');
 app.set("view engine", "jade");
+
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 var mysql = require('mysql'); // access to sql
 
+
 app.use(express.static("scripts"));  // allow the app access the scripts folder contents
 app.use(express.static("images"));   // allow the app access the images folder contents
+app.use(express.static("models"));
 
 // function to set up a simple hello response 
+
+var reviews = require("./models/reviews.json")
 
 const db = mysql.createConnection({
   
@@ -55,7 +63,7 @@ console.log(res);
 // SQL Insert DataView
 
 app.get('/insert', function (req, res){
-let sql = 'INSERT INTO products (Name, Price, Image, Category) VALUES("Maluku Quad Fish", 560, "qalukuquad.jpg", "Surfboards");'
+let sql = 'INSERT INTO products (Name, Price, Image, Category) VALUES ("Maluku Quad Fish", 560, "qalukuquad.jpg", "Surfboards");'
 let query = db.query(sql,(err,res)=>{
 if (err) throw err;
 console.log(res);
@@ -133,6 +141,141 @@ app.get('/item/:id', function(req, res){
 
 //end item page
 
+//create page
+app.get('/create', function(req, res){
+ // res.send("Hello cruel world!"); // This is commented out to allow the index view to be rendered
+  res.render('create', {root: VIEWS});
+  console.log("Now you are creating a product");
+});
+
+
+//create post request
+app.post('/create', function(req, res){
+  var name = req.body.name
+  let sql = 'INSERT INTO products (Name, Price, Image, Category) VALUES ("'+name+'", '+req.body.price+', "'+req.body.image+'", "'+req.body.category+'");'
+  let query = db.query(sql,(err,res)=>{
+  if (err) throw err;
+  console.log(res);
+  consale.log(name)
+  
+  
+});
+  
+res.render('index', {root: VIEWS});
+ 
+});
+
+
+//Edit product
+app.get('/edit/:id', function(req, res){
+ // res.send("Hello cruel world!"); // This is commented out to allow the index view to be rendered
+  let sql = 'SELECT * FROM products WHERE Id = "'+req.params.id+'";'
+  let query = db.query(sql, (err, res1) =>{
+    if(err)
+    throw(err);
+
+  res.render('edit', {root: VIEWS, res1}); // use render command  to render HTML page
+  
+});
+  console.log("You are now veiwing the edit product page!!");
+});
+
+
+//
+app.post('/edit/:id', function(req, res){
+let sql = 'UPDATE products SET Name = "'+req.body.newname+'", Price = "'+req.body.newprice+'", Category = "'+req.body.newcategory+'", Image = "'+req.body.newimage+'" WHERE Id = "'+req.params.id+'";'
+let query = db.query(sql, (err, res) =>{
+ if(err) throw err;
+ console.log(res);
+ 
+})
+
+res.redirect("/item/" + req.params.id);
+
+});
+
+
+ // function to delete database adta based on button press and form
+app.get('/delete/:id', function(req, res){
+ // res.send("Hello cruel world!"); // This is commented out to allow the index view to be rendered
+ let sql = 'DELETE FROM products WHERE Id = "'+req.params.id+'";'
+ let query = db.query(sql, (err, res1) =>{
+  if(err)
+  throw(err);
+ 
+  res.redirect('/products'); // use the render command so that the response object renders a HHTML page
+  
+ });
+ 
+ console.log("Its Gone!");
+});
+
+
+// JSON from here on
+app.get('/reviews', function(req, res){
+  res.render("reviews", {reviews:reviews})
+  
+});
+console.log("reviews on show")
+
+
+//route to render add json page
+
+app.get('/add', function(req, res){
+ // res.send("Hello cruel world!"); // This is commented out to allow the index view to be rendered
+  res.render('add', {root: VIEWS});
+  console.log("Now you are reviewing");
+});
+
+//end json
+
+
+// JSON REVIEW
+app.post('/add', function(req, res){
+	var count = Object.keys(reviews).length; // Tells us how many products we have its not needed but is nice to show how we can do this
+	console.log(count);
+	
+	// This will look for the current largest id in the reviews JSON file this is only needed if you want the reviews to have an auto ID which is a good idea 
+	
+	function getMax(reviews , id) {
+		var max
+		for (var i=0; i<reviews.length; i++) {
+			if(!max || parseInt(reviews[i][id]) > parseInt(max[id]))
+				max = reviews[i];
+			
+		}
+		return max;
+	}
+	
+	var maxPpg = getMax(reviews, "id"); // This calls the function above and passes the result as a variable called maxPpg.
+	newId = maxPpg.id + 1;  // this creates a nwe variable called newID which is the max Id + 1
+	console.log(newId); // We console log the new id for show reasons only
+	
+	// create a new product based on what we have in our form on the add page 
+	
+	var review = {
+		name: req.body.name, // name called from the add.jade page textbox
+		id: newId, // this is the variable created above
+		content: req.body.content, // content called from the add.jade page textbox
+
+	};
+		console.log(review) // Console log the new product 
+	var json  = JSON.stringify(reviews); // Convert from object to string
+	
+	// The following function reads the json file then pushes the data from the variable above to the reviews JSON file. 
+	fs.readFile('./models/reviews.json', 'utf8', function readFileCallback(err, data){
+							if (err){
+		throw(err);
+	 }else {
+		reviews.push(review); // add the information from the above variable
+		json = JSON.stringify(reviews, null , 4); // converted back to JSON the 4 spaces the json file out so when we look at it it is easily read. So it indents it. 
+		fs.writeFile('./models/reviews.json', json, 'utf8'); // Write the file back
+		
+	}});
+	res.redirect("/reviews")
+});
+
+// End JSON
 // We need to set the requirements for teh application to run
 
 app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0" , function(){
